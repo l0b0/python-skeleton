aa748cac354b67a21c2157ac05a9e33e09d7db33
Victor Engmark <victor.engmark@gmail.com>
2017-01-22 14:32:43 +0000

    Upgrade default package versions

--- configuration.mk
+++ configuration.mk
@@ -3,6 +3,6 @@ GIT ?= /usr/bin/git
 FIND ?= /usr/bin/find
 
 # Python
-PYTHON_VERSION ?= 3.5.1
-VIRTUALENV_VERSION ?= 14.0.6
+PYTHON_VERSION ?= 3.6.0
+VIRTUALENV_VERSION ?= 15.1.0
 PEP8_OPTIONS ?= --max-line-length=120
